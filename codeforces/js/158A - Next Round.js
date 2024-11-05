9c3df02a1e0d66ae2ee9255da24edb10d70594bc
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-11-05 19:47:50 -0300

    Problemas iniciais

---
+++ codeforces/js/158A - Next Round.js
@@ -0,0 +1,57 @@
+/* "Contestant who earns a score equal to or greater than the k-th place finisher's score will advance to the next round, as long as the contestant earns a positive score..." — an excerpt from contest rules.
+
+A total of n participants took part in the contest (n ≥ k), and you already know their scores. Calculate how many participants will advance to the next round.
+
+Input
+The first line of the input contains two integers n and k (1 ≤ k ≤ n ≤ 50) separated by a single space.
+
+The second line contains n space-separated integers a1, a2, ..., an (0 ≤ ai ≤ 100), where ai is the score earned by the participant who got the i-th place. The given sequence is non-increasing (that is, for all i from 1 to n - 1 the following condition is fulfilled: ai ≥ ai + 1).
+
+Output
+Output the number of participants who advance to the next round.
+
+/* Common Template Starts */
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+ 
+/* Common Template Ends */
+ 
+function main() {
+ 
+    const firstLineInput = readline().split(' ').map(Number); // [1,4,5]
+    const secondLineInputArr = readline().split(' ').map(Number); // [1,5,9]
+ 
+    const n = firstLineInput[0];
+    const k = firstLineInput[1];
+    let soma = 0;
+ 
+    for (let loop = 0; loop < n; loop++) {
+            
+        if (secondLineInputArr[loop] >= secondLineInputArr[k - 1] && secondLineInputArr[loop] > 0) {
+            soma++;  
+        }
+    }
+    console.log(soma);
+}
---
+++ codeforces/js/96A - Footbal.js
@@ -0,0 +1,71 @@
+/* Texto do problema
+
+Petya loves football very much. One day, as he was watching a football match, he was writing the players' current positions on a piece of paper. To simplify the situation he depicted it as a string consisting of zeroes and ones. A zero corresponds to players of one team; a one corresponds to players of another team. If there are at least 7 players of some team standing one after another, then the situation is considered dangerous. For example, the situation 00100110111111101 is dangerous and 11110111011101 is not. You are given the current situation. Determine whether it is dangerous or not.
+
+Input
+The first input line contains a non-empty string consisting of characters "0" and "1", which represents players. The length of the string does not exceed 100 characters. There's at least one player from each team present on the field.
+
+Output
+Print "YES" if the situation is dangerous. Otherwise, print "NO".
+
+Examples
+InputCopy
+001001
+OutputCopy
+NO
+InputCopy
+1000000001
+OutputCopy
+YES
+
+/* Common Template Starts */
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+/* Common Template Ends */
+
+function main() {
+
+    const input = readline().split('');
+
+    let resposta = '';
+    let contador_zero = 0;
+    let contador_um = 0;
+
+    for (let loop = 0; loop < input.length; loop++) {
+        if(input[loop] == '0') {
+            contador_zero++;
+            contador_um = 0;
+        } else {
+            contador_zero = 0;
+            contador_um++;
+        }
+
+        if (contador_zero == 7 || contador_um == 7) { 
+            resposta = 'YES'; 
+            loop = input.length; 
+        } else { resposta = 'NO' }
+    }
+    console.log(resposta);
+}
