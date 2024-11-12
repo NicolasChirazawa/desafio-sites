d313f1f7b50abe647139c8f2ca2db8bc4f0f398c
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-11-12 18:42:52 -0300

    Create 59A - Word.js

---
+++ codeforces/js/59A - Word.js
@@ -0,0 +1,51 @@
+/*
+Vasya is very upset that many people on the Net mix uppercase and lowercase letters in one word. That's why he decided to invent an extension for his favorite browser that would change the letters' register in every word so that it either only consisted of lowercase letters or, vice versa, only of uppercase ones. At that as little as possible letters should be changed in the word. For example, the word HoUse must be replaced with house, and the word ViP — with VIP. If a word contains an equal number of uppercase and lowercase letters, you should replace all the letters with lowercase ones. For example, maTRIx should be replaced by matrix. Your task is to use the given method on one given word.
+
+Input
+The first line contains a word s — it consists of uppercase and lowercase Latin letters and possesses the length from 1 to 100.
+
+Output
+Print the corrected word s. If the given word s has strictly more uppercase letters, make the word written in the uppercase register, otherwise - in the lowercase one.
+
+/* Common Template Starts */
+ 
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+/* Common Template Ends */
+ 
+function main() {
+    const input = readline();
+    const teste = new RegExp(/[a-z]/g);
+ 
+    let match = input.match(teste);
+ 
+       //Por conta de se o 'match' for 0 virar 'null', é necessário tratar.
+    if (match == null) { match = 0 }
+ 
+ 
+    if (match.length >= (input.length/2)) {
+        console.log(input.toLocaleLowerCase())
+    } else { console.log(input.toLocaleUpperCase()) }
+}
