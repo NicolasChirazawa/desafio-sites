fd68ae53b4c43aa4fd6ce9252eda4d7f030a70cc
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-11-16 17:03:20 -0300

    Create 492A - Vanya and Cubes.js

---
+++ codeforces/js/492A - Vanya and Cubes.js
@@ -0,0 +1,59 @@
+/*
+Vanya got n cubes. He decided to build a pyramid from them. Vanya wants to build the pyramid as follows: the top level of the pyramid must consist of 1 cube, the second level must consist of 1 + 2 = 3 cubes, the third level must have 1 + 2 + 3 = 6 cubes, and so on. Thus, the i-th level of the pyramid must have 1 + 2 + ... + (i - 1) + i cubes.
+
+Vanya wants to know what is the maximum height of the pyramid that he can make using the given cubes.
+
+Input
+The first line contains integer n (1 ≤ n ≤ 104) — the number of cubes given to Vanya.
+
+Output
+Print the maximum possible height of the pyramid in the single line.
+
+/* Common Template Starts */
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+/* Common Template Ends */
+
+function main() {
+    let input = readline();
+    let altura = 0;
+    let valor = 0;
+    let valor_2 = 0;
+    
+    do {
+        altura++;
+        //Valor atual da linha
+        valor = valor + altura;
+
+        valor_2 = valor_2 + valor;
+
+        //Exceção do 1° bloco
+        if(input < 4) { break }
+
+    } while ((valor_2 + (valor + altura + 1)) <= input)
+    
+    console.log(altura);
+}
