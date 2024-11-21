5cdbe7e96a2e48dbe1559920860aac765bc90150
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-11-20 23:16:16 -0300

    20/11/2024

---
+++ codeforces/js/78A - Haiku.js
@@ -0,0 +1,60 @@
+/*
+A word or a sentence in some language is called a pangram if all the characters of the alphabet of this language appear in it at least once. Pangrams are often used to demonstrate fonts in printing or test the output devices.
+
+You are given a string consisting of lowercase and uppercase Latin letters. Check whether this string is a pangram. We say that the string contains a letter of the Latin alphabet if this letter occurs in the string in uppercase or lowercase.
+
+Input
+The first line contains a single integer n (1 ≤ n ≤ 100) — the number of characters in the string.
+
+The second line contains the string. The string consists only of uppercase and lowercase Latin letters.
+
+Output
+Output "YES", if the string is a pangram and "NO" otherwise.
+
+/* Common Template Starts */
+ 
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+/* Common Template Ends */
+function main() {
+    const number = readline();
+    const input = readline().toLowerCase().split('');
+    let pangram = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let soma = 0;
+    
+    for(let i = 0; i < input.length; i++){
+        pangram[(input[i].charCodeAt(0)) - 97] = 1;
+    }
+ 
+    for(let i = 0; i < pangram.length; i++){
+        soma = pangram[i] + soma; 
+    }
+ 
+    if(soma == 26){
+        console.log('YES');
+    } else {
+        console.log('NO');
+    }
+}
