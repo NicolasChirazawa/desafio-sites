b69119d3bf430995c7a1fbfbd51b66bb80cf2d80
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-11-22 19:21:05 -0300

    21/11/24

---
+++ codeforces/js/510A - Fox And Snake.js
@@ -0,0 +1,97 @@
+/*
+Fox Ciel starts to learn programming. The first task is drawing a fox! However, that turns out to be too hard for a beginner, so she decides to draw a snake instead.
+
+A snake is a pattern on a n by m table. Denote c-th cell of r-th row as (r, c). The tail of the snake is located at (1, 1), then it's body extends to (1, m), then goes down 2 rows to (3, m), then goes left to (3, 1) and so on.
+
+Your task is to draw this snake for Fox Ciel: the empty cells should be represented as dot characters ('.') and the snake cells should be filled with number signs ('#').
+
+Consider sample tests in order to understand the snake pattern.
+
+Input
+The only line contains two integers: n and m (3 ≤ n, m ≤ 50).
+
+n is an odd number.
+
+Output
+Output n lines. Each line should contain a string consisting of m characters. Do not output spaces.
+*/
+/* Common Template Starts */
+ 
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+/* Common Template Ends */
+ 
+/* Tinha tentado fazer O(n) porém não consegui achar um meio de multiplicar um caractere sabido x vezes, como um (# * 4)
+ 
+function tamanho(a, b){
+    let c = '';
+    if(a % 2 == 0){
+        c = '#';
+        c = c.repeat(b);
+    } else if((a + 1) % 4 == 2) {
+        c = '.';
+        c = c.repeat(b - 1) + '#';
+    } else {
+        c = '.';
+        c = '#' + c.repeat(b - 1);
+    }
+    return c;
+}
+ 
+function main() {
+    const input = readline().split(' ');
+    let valores = [];
+ 
+    for(let i = 0; i < input[0]; i++){
+        valores = tamanho(i, input[1]);
+        console.log(valores);
+    }
+} 
+*/
+ 
+//Essa solução se aproxima mais da ideia que eu tinha pensado
+ 
+function main() {
+ 
+    const input = readline().split(' ');
+    let valores = ['', '', '#'];
+ 
+    for(let i = 0; i < input[1]; i++){
+        valores[0] += '#';
+        if(i > 0){
+            valores[1] += '.';
+        }
+    }
+ 
+    for(let j = 0; j < input[0]; j++){
+        if(j % 2 == 0){
+            console.log(valores[0]);
+        } else if((j + 1) % 4 == 2){
+            console.log(valores[1] + valores[2])
+        } else{
+            console.log(valores[2] + valores[1])
+        }
+    }
+}
