dbb88dbcea868d4d92985e137968381be5505f68
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-11-29 20:20:23 -0300

    99A - Help Far Away Kingdom (800) - (24/11/2024)

--- codeforces/js/99A - Help Far Away Kingdom.js
+++ codeforces/js/99A - Help Far Away Kingdom.js
@@ -1,3 +1,5 @@
+//99A - Help Far Away Kingdom (https://codeforces.com/contest/99/problem/A)
+
 /*
 This winter is so cold in Nvodsk! A group of n friends decided to buy k bottles of a soft drink called "Take-It-Light" to warm up a bit. Each bottle has l milliliters of the drink. Also they bought c limes and cut each of them into d slices. After that they found p grams of salt.
 
