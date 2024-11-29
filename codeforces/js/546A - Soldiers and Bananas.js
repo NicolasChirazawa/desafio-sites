2e06d80f7f788d64c96a9b7621545aa227a05100
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-11-29 19:26:28 -0300

    546A - Soldiers and Bananas (800) - (19/11/2024)

--- codeforces/js/546A - Soldiers and Bananas.js
+++ codeforces/js/546A - Soldiers and Bananas.js
@@ -1,3 +1,5 @@
+//546A - Soldiers and Bananas (https://codeforces.com/contest/546/problem/A)
+
 /*
 A soldier wants to buy w bananas in the shop. He has to pay k dollars for the first banana, 2k dollars for the second one and so on (in other words, he has to pay i·k dollars for the i-th banana).
 
