e8a897b34deca99d029a355a10f1c5e8cc390794
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-11-29 19:22:48 -0300

    49A - Sleuth (800) - (18/11/2024)

--- codeforces/js/49A - Sleuth.js
+++ codeforces/js/49A - Sleuth.js
@@ -1,3 +1,5 @@
+//49A - Sleuth (https://codeforces.com/contest/49/problem/A)
+
 /*
 Little Petya loves presents. His mum bought him two strings of the same size for his birthday. The strings consist of uppercase and lowercase Latin letters. Now Petya wants to compare those two strings lexicographically. The letters' case does not matter, that is an uppercase letter is considered equivalent to the corresponding lowercase letter. Help Petya perform the comparison.
 
