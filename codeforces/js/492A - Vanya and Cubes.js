c1df223f5c2693fc8084eccae747c5fba31610ed
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-11-29 19:18:37 -0300

    492A - Vanya and Cubes (800) - (16/11/2024)

--- codeforces/js/492A - Vanya and Cubes.js
+++ codeforces/js/492A - Vanya and Cubes.js
@@ -1,3 +1,5 @@
+//492A - Vanya and Cubes (https://codeforces.com/contest/492/problem/A)
+
 /*
 Vanya got n cubes. He decided to build a pyramid from them. Vanya wants to build the pyramid as follows: the top level of the pyramid must consist of 1 cube, the second level must consist of 1 + 2 = 3 cubes, the third level must have 1 + 2 + 3 = 6 cubes, and so on. Thus, the i-th level of the pyramid must have 1 + 2 + ... + (i - 1) + i cubes.
 
