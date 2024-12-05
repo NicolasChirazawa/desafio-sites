be92e8ab910d86913aec73ef644be8075bad7c9a
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-12-04 23:52:37 -0300

    200B - Drinks (800) - (03/12/2024)

---
+++ codeforces/js/200B - Drinks.js
@@ -0,0 +1,59 @@
+//Beautiful Matrix - 263A (https://codeforces.com/contest/263/problem/A)
+
+/*
+You've got a 5 × 5 matrix, consisting of 24 zeroes and a single number one. Let's index the matrix rows by numbers from 1 to 5 from top to bottom, let's index the matrix columns by numbers from 1 to 5 from left to right. In one move, you are allowed to apply one of the two following transformations to the matrix:
+
+Swap two neighboring matrix rows, that is, rows with indexes i and i + 1 for some integer i (1 ≤ i < 5).
+Swap two neighboring matrix columns, that is, columns with indexes j and j + 1 for some integer j (1 ≤ j < 5).
+You think that a matrix looks beautiful, if the single number one of the matrix is located in its middle (in the cell that is on the intersection of the third row and the third column). Count the minimum number of moves needed to make the matrix beautiful.
+
+Input
+The input consists of five lines, each line contains five integers: the j-th integer in the i-th line of the input represents the element of the matrix that is located on the intersection of the i-th row and the j-th column. It is guaranteed that the matrix consists of 24 zeroes and a single number one.
+
+Output
+Print a single integer — the minimum number of moves needed to make the matrix beautiful.
+
+/* Common Template Starts */
+ 
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+/* Common Template Ends */
+ 
+function main() {
+    const matriz = [];
+    let posicao = [];
+ 
+    for(let i = 0; i < 5; i++){
+        matriz[i] = readline().split(' ').join('');
+        
+        if(matriz[i] != '00000'){
+            posicao[0] = i;
+            matriz[i].split('');
+            posicao[1] = matriz[i].indexOf('1');
+        }
+    }
+    //Posição central
+    console.log(Math.abs(posicao[0] - 2) + Math.abs(posicao[1] - 2));
+}
