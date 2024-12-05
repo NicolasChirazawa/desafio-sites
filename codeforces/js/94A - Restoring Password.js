a54be71e8c06c6d4abe3203a17a2978f5e148269
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-12-04 23:50:38 -0300

    94A - Restoring Password (900) - (03/12/2024)

---
+++ codeforces/js/94A - Restoring Password.js
@@ -0,0 +1,53 @@
+//318A - Even Odds (https://codeforces.com/contest/318/problem/A) 
+
+/*
+Being a nonconformist, Volodya is displeased with the current state of things, particularly with the order of natural numbers (natural number is positive integer number). He is determined to rearrange them. But there are too many natural numbers, so Volodya decided to start with the first n. He writes down the following sequence of numbers: firstly all odd integers from 1 to n (in ascending order), then all even integers from 1 to n (also in ascending order). Help our hero to find out which number will stand at the position number k.
+
+Input
+The only line of input contains integers n and k (1 ≤ k ≤ n ≤ 1012).
+
+Please, do not use the %lld specifier to read or write 64-bit integers in C++. It is preferred to use the cin, cout streams or the %I64d specifier.
+
+Output
+Print the number that will stand at the position number k after Volodya's manipulations.
+
+
+/* Common Template Starts */
+ 
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+/* Common Template Ends */
+ 
+function main() {
+    const input = readline().split(' ').map((element) => Number(element));
+    
+    // Sempre arredonda para cima pois os ímpares "vêm primeiro" quando se conta
+    if(input[1] <= Math.round(input[0]/2)) { 
+        console.log((input[1] * 2) - 1);
+    }
+    else {
+        console.log((input[1] - Math.round(input[0]/2)) * 2)
+    }
+}
