7e3577f59bf7e3cd210b6b07f0c9ff9ef78bdf26
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-12-05 23:30:52 -0300

    177A1 - Good Matrix Elements (800) - (04/12/2024)

---
+++ codeforces/js/177A1 - Good Matrix Elements.js
@@ -0,0 +1,81 @@
+//177A2 - Good Matrix Elements (https://codeforces.com/contest/177/problem/A2)
+
+/*
+The Smart Beaver from ABBYY got hooked on square matrices. Now he is busy studying an n × n size matrix, where n is odd. The Smart Beaver considers the following matrix elements good:
+
+Elements of the main diagonal.
+Elements of the secondary diagonal.
+Elements of the "middle" row — the row which has exactly  rows above it and the same number of rows below it.
+Elements of the "middle" column — the column that has exactly  columns to the left of it and the same number of columns to the right of it.
+The figure shows a 5 × 5 matrix. The good elements are marked with green.
+Help the Smart Beaver count the sum of good elements of the given matrix.
+
+Input
+The first line of input data contains a single odd integer n. Each of the next n lines contains n integers aij (0 ≤ aij ≤ 100) separated by single spaces — the elements of the given matrix.
+
+The input limitations for getting 30 points are:
+
+1 ≤ n ≤ 5
+The input limitations for getting 100 points are:
+
+1 ≤ n ≤ 101
+Output
+Print a single integer — the sum of good matrix elements.
+
+/* Common Template Starts */
+ 
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+/* Common Template Ends */
+ 
+function main() {
+    const input = Number(readline());
+    let matriz = [];
+    let contador = 0;
+ 
+    //Populando matriz
+    for(let i = 0; i < input; i++){
+        matriz[i] = readline().split(' ').map((element) => Number(element));
+        // matriz[i] = matriz[i].split(' ').map((element) => Number(element))
+ 
+        // Diagonal principal
+        contador += matriz[i][i];
+ 
+        // Diagonal secundária
+        contador += matriz[i][input - i - 1];
+ 
+        // Elementos da coluna do meio
+        contador += matriz[i][(input - 1) / 2];
+ 
+        // Elementos da linha do meio
+        if(i == (input - 1)/ 2){
+            for(let j = 0; j < input; j++){
+                contador += matriz[i][j];
+            }
+        } 
+    }
+    //Repete 3 vezes o número do meio
+    console.log(contador - (matriz[(input - 1) / 2][(input - 1) / 2] * 3))
+}
