4473232054b67c7f5c2e731d704282c027500301
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-12-09 18:24:38 -0300

    46A - Ball Game (800) - (06/12/2024)

---
+++ codeforces/js/46A - Ball Game.js
@@ -0,0 +1,59 @@
+//46A - Ball Game (https://codeforces.com/contest/46/problem/A)
+
+/*
+A kindergarten teacher Natalia Pavlovna has invented a new ball game. This game not only develops the children's physique, but also teaches them how to count.
+
+The game goes as follows. Kids stand in circle. Let's agree to think of the children as numbered with numbers from 1 to n clockwise and the child number 1 is holding the ball. First the first child throws the ball to the next one clockwise, i.e. to the child number 2. Then the child number 2 throws the ball to the next but one child, i.e. to the child number 4, then the fourth child throws the ball to the child that stands two children away from him, i.e. to the child number 7, then the ball is thrown to the child who stands 3 children away from the child number 7, then the ball is thrown to the child who stands 4 children away from the last one, and so on. It should be mentioned that when a ball is thrown it may pass the beginning of the circle. For example, if n = 5, then after the third throw the child number 2 has the ball again. Overall, n - 1 throws are made, and the game ends.
+
+The problem is that not all the children get the ball during the game. If a child doesn't get the ball, he gets very upset and cries until Natalia Pavlovna gives him a candy. That's why Natalia Pavlovna asks you to help her to identify the numbers of the children who will get the ball after each throw.
+
+Input
+The first line contains integer n (2 ≤ n ≤ 100) which indicates the number of kids in the circle.
+
+Output
+In the single line print n - 1 numbers which are the numbers of children who will get the ball after each throw. Separate the numbers by spaces.
+
+/* Common Template Start */
+ 
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+/* Common Template Ends */
+ 
+function main(){
+    const input = Number(readline());
+    contador = 1;
+    let resposta = [];
+ 
+    for(let i = 1; i < input; i++){
+        contador += i;
+ 
+        if(contador > input){
+            contador -= input;
+        }
+        
+        resposta[i - 1] = contador;
+    }
+    console.log(resposta.join(' '));
+}
