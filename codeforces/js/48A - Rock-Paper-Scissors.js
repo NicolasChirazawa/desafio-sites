6d5f4cf2954c9740f114fdd52694a0802f181b03
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-12-09 00:16:47 -0300

    48A - Rock-paper-scissors (900) - (01/12/2024)

--- codeforces/js/48A - Rock-Paper-Scissors.js
+++ codeforces/js/48A - Rock-Paper-Scissors.js
@@ -1,13 +1,13 @@
-//34A - Reconnaissance 2 (https://codeforces.com/contest/34/problem/A)
+//48A - Rock-paper-scissors (https://codeforces.com/contest/48/problem/A)
 
 /*
-n soldiers stand in a circle. For each soldier his height ai is known. A reconnaissance unit can be made of such two neighbouring soldiers, whose heights difference is minimal, i.e. |ai - aj| is minimal. So each of them will be less noticeable with the other. Output any pair of soldiers that can form a reconnaissance unit.
+Uncle Fyodor, Matroskin the Cat and Sharic the Dog live their simple but happy lives in Prostokvashino. Sometimes they receive parcels from Uncle Fyodor’s parents and sometimes from anonymous benefactors, in which case it is hard to determine to which one of them the package has been sent. A photographic rifle is obviously for Sharic who loves hunting and fish is for Matroskin, but for whom was a new video game console meant? Every one of the three friends claimed that the present is for him and nearly quarreled. Uncle Fyodor had an idea how to solve the problem justly: they should suppose that the console was sent to all three of them and play it in turns. Everybody got relieved but then yet another burning problem popped up — who will play first? This time Matroskin came up with a brilliant solution, suggesting the most fair way to find it out: play rock-paper-scissors together. The rules of the game are very simple. On the count of three every player shows a combination with his hand (or paw). The combination corresponds to one of three things: a rock, scissors or paper. Some of the gestures win over some other ones according to well-known rules: the rock breaks the scissors, the scissors cut the paper, and the paper gets wrapped over the stone. Usually there are two players. Yet there are three friends, that’s why they decided to choose the winner like that: If someone shows the gesture that wins over the other two players, then that player wins. Otherwise, another game round is required. Write a program that will determine the winner by the gestures they have shown.
 
 Input
-The first line contains integer n (2 ≤ n ≤ 100) — amount of soldiers. Then follow the heights of the soldiers in their order in the circle — n space-separated integers a1, a2, ..., an (1 ≤ ai ≤ 1000). The soldier heights are given in clockwise or counterclockwise direction.
+The first input line contains the name of the gesture that Uncle Fyodor showed, the second line shows which gesture Matroskin showed and the third line shows Sharic’s gesture.
 
 Output
-Output two integers — indexes of neighbouring soldiers, who should form a reconnaissance unit. If there are many optimum solutions, output any of them. Remember, that the soldiers stand in a circle.
+Print "F" (without quotes) if Uncle Fyodor wins. Print "M" if Matroskin wins and "S" if Sharic wins. If it is impossible to find the winner, print "?".
 
 /* Common Template Starts */
  
@@ -37,34 +37,54 @@ function readline() {
 }
 /* Common Template Ends */
  
-function menorValor(a, b){
-    return a[1] - b[1];
-}
+//The first input line contains the name of the gesture that Uncle Fyodor showed, the second line shows which gesture Matroskin showed and the third line shows Sharic’s gesture.
+//Print "F" (without quotes) if Uncle Fyodor wins. Print "M" if Matroskin wins and "S" if Sharic wins. If it is impossible to find the winner, print "?".
  
 function main() {
  
-    const input = readline();
-    const alturas = readline().split(' ');
-    let contador = 1;
+    let ordem = [];
+    let jogadores = ['F', 'M', 'S'];
+    let resposta = [0, 0, 0];
  
-    //Declara dicionário (pode ser '{}' também)
-    let dict = new Object();
+    for(let i = 0; i < 3; i++){
+        ordem[i] = readline();
  
-    for(let i = 0; i < Number(input); i++){
-    
-        //Ultima combinação
-        if(contador == Number(input)) { contador = 0 }
+        if(ordem[i] == 'paper'){
+            resposta[0]++;
+        } else if(ordem[i] == 'rock'){
+            resposta[1]++;
+        } else {
+            resposta[2]++;
+        }
  
-        dict[i + 1] = Math.abs(alturas[i] - alturas[contador]);
-        contador++;
+        if(i == 2){
+            resposta.join('');
+        }
     }
-    //Pegar conjunto chave-valor
-    let dict_array = Object.entries(dict);
-    dict_array = dict_array.sort(menorValor);
- 
-    if(Number(dict_array[0][0]) == Number(input)){
-        console.log(input + ' ' + 1)
+    if(resposta.indexOf('2' != -1)){
+        if(resposta[0] == '1' && resposta[1] == '2'){
+            for(let i = 0; i < 3; i++){
+                if(ordem[i] == 'paper'){
+                    console.log(jogadores[i]);
+                }
+            }
+        } else if(resposta[1] == '1' && resposta[2] == '2') {
+            for(let i = 0; i < 3; i++){
+                if(ordem[i] == 'rock'){
+                    console.log(jogadores[i]);
+                }
+            }
+        } else if(resposta[2] == '1' && resposta[0] == '2') {
+            for(let i = 0; i < 3; i++){
+                if(ordem[i] == 'scissors'){
+                    console.log(jogadores[i]);
+                }
+            }
+        }
+        else {
+            console.log('?');
+        }
     } else {
-        console.log(dict_array[0][0] + ' ' + (Number(dict_array[0][0]) + 1))
+        console.log('?');
     }
 }
