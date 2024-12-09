eb9071c90494607256cc2ca7d29a21f57e284ae8
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-12-09 17:51:23 -0300

    11A - Increasing Sequence (900) - (01/12/24)

--- codeforces/js/11A - Increasing Sequence.js
+++ codeforces/js/11A - Increasing Sequence.js
@@ -1,13 +1,15 @@
-//52A - 123-Sequence (https://codeforces.com/contest/52/problem/A)
+//11A - Increscing Sequence (https://codeforces.com/contest/11/problem/A)
 
 /*
-There is a given sequence of integers a1, a2, ..., an, where every number is from 1 to 3 inclusively. You have to replace the minimum number of numbers in it so that all the numbers in the sequence are equal to each other.
+A sequence a0, a1, ..., at - 1 is called increasing if ai - 1 < ai for each i: 0 < i < t.
+
+You are given a sequence b0, b1, ..., bn - 1 and a positive integer d. In each move you may choose one element of the given sequence and add d to it. What is the least number of moves required to make the given sequence increasing?
 
 Input
-The first line contains an integer n (1 ≤ n ≤ 106). The second line contains a sequence of integers a1, a2, ..., an (1 ≤ ai ≤ 3).
+The first line of the input contains two integer numbers n and d (2 ≤ n ≤ 2000, 1 ≤ d ≤ 106). The second line contains space separated sequence b0, b1, ..., bn - 1 (1 ≤ bi ≤ 106).
 
 Output
-Print the minimum number of replacements needed to be performed to make all the numbers in the sequence equal.
+Output the minimal number of moves needed to make the sequence increasing.
 
 /* Common Template Starts */
  
@@ -39,20 +41,33 @@ function readline() {
  
 function main() {
  
-    const input = readline();
+    //'n' e 'd'
+    const input = readline().split(' ');
+    //Valores de entrada
     let sequencia = readline().split(' ');
-    let contador = [0, 0, 0]
  
-    for(let i = 0; i < input; i++){
-        if(Number(sequencia[i]) == 1){
-            contador[0]++;
-        } else if (Number(sequencia[i]) == 2){
-            contador[1]++;
-        } else {
-            contador[2]++;
+    let moves = 0;
+ 
+    for(let i = 1; i < input[0]; i++){
+        if(Number(sequencia[i]) == Number(sequencia[i - 1])){
+ 
+            sequencia[i] = Number(sequencia[i]) + Number(input[1]);
+            moves++;
+ 
+        } else if(Number(sequencia[i]) < Number(sequencia[i - 1])){
+            
+            let diferenca = Number(sequencia[i - 1]) - Number(sequencia[i]);
+ 
+            //O 'Math.ceil' serve para lidar com números quebrados, todo número quebrado deve tender para cima, um jeito fácil de pensar nisso, é ver números 0,5, no mínimo, precisa ser 1 movimento completo.
+            let multVezes = Math.ceil(diferenca / input[1]);
+ 
+            //Existe o caso que ambos são iguais, ou seja 529 e 529.
+            if((Number(sequencia[i])) + (multVezes * input[1]) == Number(sequencia[i - 1])) { multVezes++ }
+ 
+            sequencia[i] = Number(sequencia[i]) + (multVezes * input[1]);
+ 
+            moves = moves + multVezes;
         }
     }
- 
-    let maior = Math.max(contador[0], contador[1], contador[2]);
-    console.log(input - maior);
+    console.log(moves);
 }
