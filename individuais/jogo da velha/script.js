468e747f76bf1665f2f2717782006b350e49ac0f
NicolasChirazawa <117954184+NicolasChirazawa@users.noreply.github.com>
2024-12-16 00:29:10 -0300

    Contador funcionando

--- individuais/jogo da velha/script.js
+++ individuais/jogo da velha/script.js
@@ -13,7 +13,10 @@ const jogo = {
     lance: 1,
     quantidadeLance: 0,
     resultado: JOGO_EM_ANDAMENTO,
-    
+}
+
+const resultado = {
+
     /*
         1: Empate
         2: Vitória do X
@@ -21,6 +24,7 @@ const jogo = {
     */
     score: [0, 0, 0],
 }
+
 
 addEventListener("click", (e) => {
 
@@ -41,12 +45,12 @@ addEventListener("click", (e) => {
         return 
     };
 
-    //Caso caia num return, independente do resultado, vai entrar no if
+    // Caso caia num return, independente do resultado, vai entrar no if
     let conclusao = condicaoVitoria();
 
-    console.log(conclusao);
-
     if(conclusao == 'Vitória do O' || conclusao == 'Vitória do X' || conclusao == 'Empate'){
+        pontuador(jogo.resultado);
+        mudarFrasePontuador(jogo.resultado);
         mudarTextoLinha(jogo.resultado);
         gerarBotaoReiniciar();
     }
@@ -57,7 +61,7 @@ function mudarTextoLinha (texto) {
     frase.innerHTML = `${texto}`
 }
 
-function jogada(valor){
+function jogada(valor) {
     
     if (jogo.posicaoValor[valor[1] - 1] != SEM_LANCE) { 
         console.log('teste')
@@ -79,7 +83,7 @@ function mudarCaractere (valor) {
     }
 }
 
-function passarLance(){
+function passarLance() {
     if(jogo.lance % 2 == 0){
         jogo.lance = 1;
         mudarTextoLinha("Vez do X");
@@ -89,7 +93,7 @@ function passarLance(){
     }
 }
 
-function condicaoVitoria(){
+function condicaoVitoria() {
     
     // Vitória
     for(let i = 0; i < jogo.posicaoValor.length; i++){
@@ -99,14 +103,15 @@ function condicaoVitoria(){
 
                 console.log('horizontal');
                 jogo.resultado = j % 2 == 0 ? 'Vitória do O' : 'Vitória do X';
+                
                 return jogo.resultado;
             }
 
         // Verificação vertical
             if (jogo.posicaoValor[0 + i] == j && jogo.posicaoValor[3 + i] == j && jogo.posicaoValor[6 + i] == j){
-
                 console.log('vertical');
                 jogo.resultado = j % 2 == 0 ? 'Vitória do O' : 'Vitória do X';
+                
                 return jogo.resultado;
             }
         }
@@ -116,25 +121,66 @@ function condicaoVitoria(){
         if(jogo.posicaoValor[0] == i && jogo.posicaoValor[4] == i && jogo.posicaoValor[8] == i){
             console.log("diagonal");
             jogo.resultado = i % 2 == 0 ? 'Vitória do O' : 'Vitória do X';
+            
             return jogo.resultado;
         }
         if(jogo.posicaoValor[2] == i && jogo.posicaoValor[4] == i && jogo.posicaoValor[6] == i){
             console.log("diagonal");
             jogo.resultado = i % 2 == 0 ? 'Vitória do O' : 'Vitória do X';
+            
             return jogo.resultado;
         }
     }
 
-    // Empate
+    // Contador de lance para testar empate
     jogo.quantidadeLance++;
 
+    // Empate
     if(jogo.quantidadeLance == 9) { 
         jogo.resultado = 'Empate'; 
+        
         return jogo.resultado;
     }
 }
 
-function gerarBotaoReiniciar(){
+function pontuador(conclusao) {
+
+    if(conclusao == 'Vitória do X'){
+        resultado.score[1]++;
+    } else if (conclusao == 'Vitória do O'){
+        resultado.score[2]++;
+    } else {
+        resultado.score[0]++;
+    }
+}
+
+function mudarFrasePontuador(conclusao){
+    
+    //Transformar o 'possibilidades' num hashmap para organização
+
+    let resposta = '';
+    let contador = 0;
+    let possibilidades = [['Empate', "empate"], ['Vitória do X', "pontoX"], ['Vitória do O', "pontoO"]];
+
+    // Descobrindo quem mudou
+    for(let i = 0; i < possibilidades.length; i++){
+        if(possibilidades[i][0] == conclusao){
+            resposta = possibilidades[i];
+            break;
+        }
+        contador++;
+    }
+
+    console.log(resposta);
+    console.log(contador);
+
+    let frase = document.getElementById(`${resposta[1]}`);
+    frase.innerHTML = resposta[0] + ": " + resultado.score[contador];
+
+    //console.log(frase);
+}
+
+function gerarBotaoReiniciar() {
     const novoBotao = document.createElement("button");
     const textoBotao = document.createTextNode("Reiniciar");
 
@@ -148,19 +194,19 @@ function gerarBotaoReiniciar(){
     novoBotao.addEventListener("click", () => {
         reiniciar();
         novoBotao.remove();
-        mudarTextoLinha("Nova partida")
+        mudarTextoLinha("Nova partida");
     })
 }
 
-function reiniciar(){    
+function reiniciar() {   
     jogo.posicaoValor = [SEM_LANCE, SEM_LANCE, SEM_LANCE, SEM_LANCE, SEM_LANCE, SEM_LANCE, SEM_LANCE, SEM_LANCE, SEM_LANCE]
 
     for(let i = 0; i < (jogo.posicaoValor.length); i++){
         let textoInterno = document.getElementById(`${i + 1}`);
 
         textoInterno.children[0].innerHTML = '';
-        jogo.resultado = JOGO_EM_ANDAMENTO;
+        jogo.lance = 1;
         jogo.quantidadeLance = 0;
+        jogo.resultado = JOGO_EM_ANDAMENTO;
     }
-    
 }
