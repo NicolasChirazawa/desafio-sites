1071ef546bdadfa66bc70102e9700ead2625fecb
Nicolas Fernandes <nicolaschfernandes@gmail.com>
2025-01-02 17:53:36 -0300

    443A - Anton and Letters (1000) - (27/11/2024)

---
+++ codeforces/js/443A - Anton and Letters.js
@@ -0,0 +1,53 @@
+// 443A - Anton and Letters (https://codeforces.com/contest/443/problem/A)
+
+/*
+Recently, Anton has found a set. The set consists of small English letters. Anton carefully wrote out all the letters from the set in one line, separated by a comma. He also added an opening curved bracket at the beginning of the line and a closing curved bracket at the end of the line.
+
+Unfortunately, from time to time Anton would forget writing some letter and write it again. He asks you to count the total number of distinct letters in his set.
+
+Input
+The first and the single line contains the set of letters. The length of the line doesn't exceed 1000. It is guaranteed that the line starts from an opening curved bracket and ends with a closing curved bracket. Between them, small English letters are listed, separated by a comma. Each comma is followed by a space.
+
+Output
+Print a single number — the number of distinct letters in Anton's set.
+
+/* Common Template Starts */
+ 
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+ 
+let inputString = "";
+let currentLine = 0;
+ 
+process.stdin.on("data", (inputStdin) => {
+    inputString += inputStdin;
+});
+ 
+process.stdin.on("end", (_) => {
+    inputString = inputString
+        .trim()
+        .split("\n")
+        .map((string) => {
+            return string.trim();
+        });
+ 
+    main();
+});
+ 
+function readline() {
+    return inputString[currentLine++];
+}
+/* Common Template Ends */
+ 
+function main() {
+    let input = readline();
+ 
+    if(input == '{}') { 
+        console.log(0);
+    } else {
+        input = input.slice(1, input.length - 1).split(', ');
+ 
+        let resposta = [...new Set(input)];
+        console.log(resposta.length);
+    }
+}
